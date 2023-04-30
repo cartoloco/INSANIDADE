5a1a71afb7e9802cc77b2ea2aeb27024d779e56e
cartoloco <132097799+cartoloco@users.noreply.github.com>
2023-04-30 14:42:27 -0300

    Update script.js

--- js2/script.js
+++ js2/script.js
@@ -2,7 +2,7 @@ function verificar() {
     const num1 = parseInt(document.getElementById("caixa1").value);
     const num2 = parseInt(document.getElementById("caixa2").value);
     const num3 = parseInt(document.getElementById("caixa3").value);
-    if (clicksAmarelo === 9 && clicksVerde === 3 && clicksVermelho === 4) {
+    if (clicksAmarelo === 9 && clicksVerde === 2 && clicksVermelho === 4) {
         document.getElementById("mensagem").textContent = "A FENDA!";
         window.location.href = "./index3.html";
     } else {
